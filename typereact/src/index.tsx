45419304880efe67d48c814a651ddc68c902273b
Chanwoo-Jeong <lordroad38@naver.com>
2023-02-21 19:58:18 +0900

    study styled-components & typeReact

---
+++ typereact/src/index.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ThemeProvider } from 'styled-components';
+
+const darkMode = {
+  textColor : "white",
+  backgroundColor : "black"
+} 
+
+const lightMode = {
+  textColor : "black",
+  backgroundColor : "whitesmoke"
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    {/* <ThemeProvider theme={darkMode}> */}
+    <App />
+    {/* </ThemeProvider> */}
+  </React.StrictMode>
+);
+
