de2ce435b0079c53a3f62b05cac4b03a25713ac8
Chanwoo-Jeong <lordroad38@naver.com>
2023-06-18 23:30:39 +0900

    [ feat ] : change Category Recoil

--- todolist/src/Components/ToDo.tsx
+++ todolist/src/Components/ToDo.tsx
@@ -1,12 +1,41 @@
 import React from "react";
-import { IToDo } from "./atoms";
+import { IToDo, toDoState } from "./atoms";
+import { useSetRecoilState } from "recoil";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onclick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const { name } = e.currentTarget;
+
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
+
   return (
     <li>
-      {text} <button>Todo</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {text}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onclick}>
+          Todo
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onclick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onclick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
